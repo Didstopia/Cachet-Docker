10009c902863385423838c9e1a071cb40326819f
Pauli Jokela <pauli.jokela@didstopia.com>
2016-06-11 22:29:12 +0300

    Pushing changes

--- docker_push.sh
+++ docker_push.sh
@@ -3,5 +3,5 @@
 # Set Docker to use the machine
 eval "$(docker-machine env default)"
 
-docker tag -f didstopia/cachet:latest didstopia/serum:latest
+docker tag -f didstopia/cachet:latest didstopia/cachet:latest
 docker push didstopia/cachet:latest
