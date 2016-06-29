389121b037976d6f20dba75dcc97442660fd195e
Pauli Jokela <pauli.jokela@didstopia.com>
2016-06-29 12:56:00 +0300

    Pushing changes

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Set Docker to use the machine
-eval "$(docker-machine env default)"
-
-docker tag -f didstopia/cachet:latest didstopia/cachet:latest
+docker tag didstopia/cachet:latest didstopia/cachet:latest
 docker push didstopia/cachet:latest
--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Set Docker to use the machine
-eval "$(docker-machine env default)"
-
 docker run -p 80:8000 --name cachet -it didstopia/cachet:latest bash
